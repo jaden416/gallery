fd8f4aa6a39e38de0bb5d713560f2c2466f893ef
jaden416 <jaden416@outlook.com>
2024-01-08 10:37:50 -0500

    fixed error

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,18 +12,10 @@ export default function Home() {
           <div className="absolute inset-x-[-150%] s:inset-x-[-50%] l:inset-x-[-30%] xl:inset-x-[-17.5%] top-0 grid grid-cols-6 cursor-move js-grid-bounds">
           {data.map((media, index)=>(
               <div key={index} className="relative">
-                <div className="aspect" ></div>
-                <div className="absolute inset-5 js-tile" data-src={media.src}></div>
+                <div className="aspect"  ></div>
+                <div className="absolute inset-5 js-tile " data-src={media.src}></div>
               </div>
-            ))}Home
-Work
-ArchiveHome
-Work
-Archive
-About
-Font pairings for SquarespaceBrandingArt Direction for Violent White SupremacyAthens, GreeceTask randomiserFont pairings for SquarespaceHa Giang LoopUnfold PromoPhotography for At HomeUnfold templateOld portfolio about pageUnfold TemplateE-commerace websiteJoshua TreeAbandoned hospital, SchönwaldeMoon Light CoffeeArt Direction for Jigsaw IssueLanding page for old portfolioCostal Waters - Event PosterColour by WordsBranding for a now deceased teamAn Account of Corsica - Title ExplorationThe Current Landing PageMoonlight Coffee business card
-About
-Font pairings for SquarespaceBrandingArt Direction for Violent White SupremacyAthens, GreeceTask randomiserFont pairings for SquarespaceHa Giang LoopUnfold PromoPhotography for At HomeUnfold templateOld portfolio about pageUnfold TemplateE-commerace websiteJoshua TreeAbandoned hospital, SchönwaldeMoon Light CoffeeArt Direction for Jigsaw IssueLanding page for old portfolioCostal Waters - Event PosterColour by WordsBranding for a now deceased teamAn Account of Corsica - Title ExplorationThe Current Landing PageMoonlight Coffee business card
+            ))}
           </div>
           <Canvas
             camera={
