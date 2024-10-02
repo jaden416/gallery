92a0ec17290c34e0ceda4612a73a897cf2feed27
Jaden Lewis-Chambers <you@example.com>
2024-10-02 16:34:07 -0400

    saved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { urls } from '../../data/gallery.js'
-import HomeScene from '../../three/home/Home.jsx'
-import { Canvas } from '@react-three/fiber'
-import * as THREE from 'three'
+import React, { useEffect, useState } from "react";
+import { urls } from "../../data/gallery.js";
+import HomeScene from "../../three/home/HomeScene.jsx";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Home() {
   const data = urls;
@@ -11,51 +11,62 @@ export default function Home() {
   const textureLoader = new THREE.TextureLoader();
 
   useEffect(() => {
-    const array = [...document.querySelectorAll('.js-tile')];
+    const array = [...document.querySelectorAll(".js-tile")];
     const loadedTextures = array.map((element) =>
-      textureLoader.load(element.getAttribute('data-src'))
+      textureLoader.load(element.getAttribute("data-src"))
     );
     setTextures(loadedTextures);
   }, []);
-  
-  return <>
+
+  return (
+    <>
       <main>
-      <article>
-        <div className="fixed bottom-0 left-0 h-extra-screen w-full pt-40 flex items-center justify-center bg-white overflow-hidden rounded-t-[4rem] js-t-mask js-grid">
-          <div className="absolute inset-x-[-150%] s:inset-x-[-50%] l:inset-x-[-30%] xl:inset-x-[-17.5%] top-0 grid grid-cols-6 cursor-move js-grid-bounds">
-          {data.map((media, index)=>(
-              <div 
-                key={index} 
-                data-index={index} 
-                className="relative">
-                <div className="aspect"  ></div>
-                <div className="absolute inset-5 js-tile " data-index={index} data-src={media.src}></div>
+        <article>
+          <div className="fixed bottom-0 left-0 h-extra-screen w-full pt-40 flex items-center justify-center bg-white overflow-hidden rounded-t-[4rem] js-t-mask js-grid">
+            <div className="absolute inset-x-[-150%] s:inset-x-[-50%] l:inset-x-[-30%] xl:inset-x-[-17.5%] top-0 grid grid-cols-6 cursor-move js-grid-bounds">
+              {data.map((media, index) => (
+                <div key={index} data-index={index} className="relative">
+                  <div className="aspect"></div>
+                  <div
+                    className="absolute inset-5 js-tile "
+                    data-index={index}
+                    data-src={media.src}
+                  ></div>
+                </div>
+              ))}
+            </div>
+            <div className="pointer-events-none	 relative z-10 flex flex-col items-center justify-center">
+              <img
+                src="/src/assets/k/Font-Pairings-01.jpg"
+                className="invisible pointer-events-none	 h-[60rem] w-auto grid-focused js-grid-focused"
+              />
+              <div className="relative min-w-[24rem] h-[5rem] js-grid-content">
+                {data.map((item, index) => {
+                  return (
+                    <p
+                      key={index}
+                      className="absolute top-4 left-0 w-full text-18 opacity-0 text-center text-md js-grid-text"
+                    >
+                      {item.text}
+                    </p>
+                  );
+                })}
               </div>
-            ))}
+            </div>
+            <div className="absolute h-full w-full">
+              <Canvas
+                className="gl z-5"
+                camera={{
+                  fov: 45,
+                  zoom: 1,
+                }}
+              >
+                <HomeScene textures={textures} />
+              </Canvas>
+            </div>
           </div>
-          <div className="pointer-events-none	 relative z-10 flex flex-col items-center justify-center">
-            <img src="/src/assets/k/Font-Pairings-01.jpg" className="invisible pointer-events-none	 h-[60rem] w-auto grid-focused js-grid-focused"/>
-          </div>
-          <div className="absolute h-full w-full">
-
-          <Canvas
-            className='gl z-5'
-            camera={
-              {
-                fov: 45,
-                zoom: 1 
-              }
-              
-            }
-          >
-            <HomeScene
-              textures={textures}
-            />
-          </Canvas>
-          </div>
-
-        </div>
-      </article>
-    </main>
-  </>
+        </article>
+      </main>
+    </>
+  );
 }
--- src/three/home/Home.jsx
+++ src/three/home/Home.jsx
@@ -1,206 +1,195 @@
-import React from 'react'
-import { useEffect,  useRef, useState } from 'react';
-import { useThree, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
-import gsap from 'gsap'
-import Media from './Media';
-import normalizeWheel from 'normalize-wheel'
-import useTouchEvents from '../../hooks/useTouchEvents'
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+import { useThree, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import gsap from "gsap";
+import Media from "./Media";
+import normalizeWheel from "normalize-wheel";
+import useTouchEvents from "../../hooks/useTouchEvents";
 
-export default function HomeScene({textures}) {
-  const [gallery, setGallery] = useState(null)
-  const [medias, setMedias] = useState(null)
-  const [focusedTile, setFocusedTile] = useState(null)
+export default function HomeScene({ textures }) {
+  const [gallery, setGallery] = useState(null);
+  const [medias, setMedias] = useState(null);
+  const [focusedTile, setFocusedTile] = useState(null);
   const [visible, setVisible] = useState({
     index: null,
     state: true,
-  })
-
+  });
 
   const scroll = useRef({
-      x : {
+    x: {
       target: 0,
       current: 0,
       ease: 0.1,
       last: 0,
       position: 0,
       start: 0,
-      direction: 'right'
+      direction: "right",
     },
-    y : {
+    y: {
       target: 0,
       current: 0,
       ease: 0.1,
       last: 0,
       position: 0,
       start: 0,
-      direction: 'bottom'
-    }
-  })
+      direction: "bottom",
+    },
+  });
 
   const speed = useRef({
     target: 0,
     current: 0,
-    ease: 0.1
-  })
-  
-  const isDown = useRef(false)
+    ease: 0.1,
+  });
 
-  const hit = useRef(null) // used to briefly identify the plane index the user clicks on
+  const isDown = useRef(false);
 
+  const hit = useRef(null); // used to briefly identify the plane index the user clicks on
 
-  const {scene, camera, raycaster, pointer} = useThree()
-
+  const { scene, camera, raycaster, pointer } = useThree();
 
   const planeGeometry = new THREE.PlaneGeometry(1, 1, 20, 20);
 
-  useEffect(()=>{
-    setGallery(document.querySelector('.js-grid-bounds'))
-    setMedias([...document.querySelectorAll('.js-tile')])
-    setFocusedTile(document.querySelector('.js-grid-focused'))
-  },[])
+  useEffect(() => {
+    setGallery(document.querySelector(".js-grid-bounds"));
+    setMedias([...document.querySelectorAll(".js-tile")]);
+    setFocusedTile(document.querySelector(".js-grid-focused"));
+  }, []);
 
-  useFrame(()=>{
-
+  useFrame(() => {
     scroll.current.y.current = gsap.utils.interpolate(
       scroll.current.y.current,
       scroll.current.y.target,
       scroll.current.y.ease
-    ) 
+    );
 
     scroll.current.x.current = gsap.utils.interpolate(
       scroll.current.x.current,
       scroll.current.x.target,
       scroll.current.x.ease
-    ) 
-    
-    speed.current.target = (scroll.current.y.target - scroll.current.y.current) * .001
+    );
 
+    speed.current.target =
+      (scroll.current.y.target - scroll.current.y.current) * 0.001;
 
     speed.current.current = gsap.utils.interpolate(
-      speed.current.current ,
+      speed.current.current,
       speed.current.target,
       speed.current.ease
     );
-    
+
     if (scroll.current.x.current > scroll.current.x.last) {
-      scroll.current.x.direction = 'left';
+      scroll.current.x.direction = "left";
     } else if (scroll.current.x.current < scroll.current.x.last) {
-      scroll.current.x.direction = 'right';
+      scroll.current.x.direction = "right";
     }
-    scroll.current.x.last = scroll.current.x.current
+    scroll.current.x.last = scroll.current.x.current;
 
     if (scroll.current.y.current > scroll.current.y.last) {
-      scroll.current.y.direction = 'top';
+      scroll.current.y.direction = "top";
     } else if (scroll.current.y.current < scroll.current.y.last) {
-      scroll.current.y.direction = 'bottom';
-
+      scroll.current.y.direction = "bottom";
     }
-    scroll.current.y.last = scroll.current.y.current
-  })
+    scroll.current.y.last = scroll.current.y.current;
+  });
 
-  const onWheel = (event) =>{
-    if(!visible.state) return
+  const onWheel = (event) => {
+    if (!visible.state) return;
 
-    const { pixelY } = normalizeWheel(event)
+    const { pixelY } = normalizeWheel(event);
 
-    scroll.current.y.target += pixelY / 6
-  }
+    scroll.current.y.target += pixelY / 6;
+  };
 
   const onTouchDown = (event) => {
-    if(!visible.state) return
-    isDown.current = true
+    if (!visible.state) return;
+    isDown.current = true;
 
-    scroll.current.x.position = scroll.current.x.current
-    scroll.current.y.position = scroll.current.y.current
+    scroll.current.x.position = scroll.current.x.current;
+    scroll.current.y.position = scroll.current.y.current;
 
+    scroll.current.x.start = event.touches
+      ? event.touches[0].clientX
+      : event.clientX;
 
-    scroll.current.x.start  = event.touches
-    ? event.touches[0].clientX
-    : event.clientX
-
-    scroll.current.y.start  = event.touches
+    scroll.current.y.start = event.touches
       ? event.touches[0].clientY
-      : event.clientY
+      : event.clientY;
+  };
 
+  const onTouchMove = (event) => {
+    if (!isDown.current || !visible.state) return;
 
-  }
+    const x = event.touches ? event.touches[0].clientX : event.clientX;
 
-  const onTouchMove = (event) =>{
-    if (!isDown.current || !visible.state) return
+    const y = event.touches ? event.touches[0].clientY : event.clientY;
 
-    const x = event.touches
-    ? event.touches[0].clientX
-    : event.clientX
-    
-    const y = event.touches
-    ? event.touches[0].clientY
-    : event.clientY
+    const distanceX = scroll.current.x.start - x;
+    const distanceY = scroll.current.y.start - y;
 
-    const distanceX = scroll.current.x.start - x
-    const distanceY = scroll.current.y.start - y
+    scroll.current.x.target = scroll.current.x.position + -distanceX;
+    scroll.current.y.target = scroll.current.y.position + distanceY;
+  };
 
-    scroll.current.x.target = (scroll.current.x.position + -distanceX)
-    scroll.current.y.target = (scroll.current.y.position + distanceY)
+  const onTouchUp = () => {
+    isDown.current = false;
 
-  }
-
-  const onTouchUp = () =>{
-
-    isDown.current = false
-
-
-    if(Math.round(scroll.current.x.target) == Math.round(scroll.current.x.position)){
+    if (
+      Math.round(scroll.current.x.target) ==
+      Math.round(scroll.current.x.position)
+    ) {
       raycaster.setFromCamera(pointer, camera);
       const intersects = raycaster.intersectObjects(scene.children);
 
-      if(intersects.length > 0){
-        const obj = intersects[0].object
-        hit.current = obj.index
+      if (intersects.length > 0) {
+        const obj = intersects[0].object;
+        hit.current = obj.index;
 
-        console.log(obj.index)
+        console.log(obj.index);
 
-        visible.state ? onOpen(hit.current) : onClose() // click
-      }else{
-        onClose() // will change later
+        visible.state ? onOpen(hit.current) : onClose(); // click
+      } else {
+        onClose(); // will change later
       }
     }
-  }
+  };
 
-  const onOpen = (index) =>{
+  const onOpen = (index) => {
     setVisible({
       index,
-      state: false
-    })
-  }
+      state: false,
+    });
+  };
 
-  const onClose = () =>{
+  const onClose = () => {
     setVisible({
       index: null,
-      state: true
-    })
-  }
+      state: true,
+    });
+  };
 
-  useTouchEvents(onWheel, onTouchDown, onTouchMove, onTouchUp)
+  useTouchEvents(onWheel, onTouchDown, onTouchMove, onTouchUp);
   if (medias == null) return null;
 
-  return <>
-    {medias.map((media, index) =>
-      <Media
-        key={index}
-        index={index} // important for the raycaster
-        column={index % 6}
-        element={media}
-        galleryElement={gallery}
-        geometry={planeGeometry}
-        visible={visible}
-        scroll={scroll.current}
-        speed = {speed.current}
-        isDown={isDown}
-        texture={textures[index]}
-        focus ={focusedTile}
-      />
-    )}
-  </>
-  
+  return (
+    <>
+      {/* {medias.map((media, index) =>
+      // <Media
+      //   key={index}
+      //   index={index} // important for the raycaster
+      //   column={index % 6}
+      //   element={media}
+      //   galleryElement={gallery}
+      //   geometry={planeGeometry}
+      //   visible={visible}
+      //   scroll={scroll.current}
+      //   speed = {speed.current}
+      //   isDown={isDown}
+      //   texture={textures[index]}
+      //   focus ={focusedTile}
+      // />
+    )} */}
+    </>
+  );
 }
